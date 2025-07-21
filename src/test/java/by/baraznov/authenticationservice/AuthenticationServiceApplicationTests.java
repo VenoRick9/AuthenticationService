437ca5b2ea8645e61009037c2ed775f9646c77b5
VenoRick9 <danila.baraznou@innowise.com>
2025-07-21 11:41:15 +0300

    edit ci

--- src/test/java/by/baraznov/authenticationservice/AuthenticationServiceApplicationTests.java
+++ src/test/java/by/baraznov/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -4,11 +4,13 @@ import by.baraznov.authenticationservice.config.TestContainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Import(TestContainersConfig.class)
 @Testcontainers
+@ActiveProfiles("test")
 class AuthenticationServiceApplicationTests {
 
     @Test
--- src/test/java/by/baraznov/authenticationservice/securities/JwtProviderTest.java
+++ src/test/java/by/baraznov/authenticationservice/securities/JwtProviderTest.java
@@ -20,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 class JwtProviderTest {
 
     private JwtProvider jwtProvider;
