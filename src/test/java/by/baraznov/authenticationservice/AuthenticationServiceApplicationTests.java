696ec6f455e26433e260e8dcb33a59cfb9f3c448
VenoRick9 <danila.baraznou@innowise.com>
2025-07-21 11:44:51 +0300

    edit ci

--- src/test/java/by/baraznov/authenticationservice/AuthenticationServiceApplicationTests.java
+++ src/test/java/by/baraznov/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -4,13 +4,11 @@ import by.baraznov.authenticationservice.config.TestContainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Import(TestContainersConfig.class)
 @Testcontainers
-@ActiveProfiles("test")
 class AuthenticationServiceApplicationTests {
 
     @Test
--- src/test/java/by/baraznov/authenticationservice/controllers/AuthControllerTest.java
+++ src/test/java/by/baraznov/authenticationservice/controllers/AuthControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -30,7 +29,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Import(TestContainersConfig.class)
 @Testcontainers
-@ActiveProfiles("test")
 class AuthControllerTest {
 
     @Autowired
